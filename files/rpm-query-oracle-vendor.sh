0b53045ade8a3689b3283f90382b2ad7767a2018
Mike Savage <mike.savage@gmail.com>
2024-03-28 12:24:34 -0400

    Switch to cat of original out list to preserve newlines in STDOUT.

--- files/rpm-query-oracle-vendor.sh
+++ files/rpm-query-oracle-vendor.sh
@@ -4,5 +4,5 @@ RPM_VENDOR_LIST=$(grep 'Oracle America' /tmp/rpm_vendor_list.out)
 if [[ -z "$RPM_VENDOR_LIST" ]]; then
     echo "No Oracle America packages present"
 else
-    cat /tmp/rpm_vendor_list.out
+    grep 'Oracle America' /tmp/rpm_vendor_list.out
 fi
