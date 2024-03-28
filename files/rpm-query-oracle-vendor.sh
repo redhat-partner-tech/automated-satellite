9e96186625d9a505a5fba58610b848538c016e35
Mike Savage <mike.savage@gmail.com>
2024-03-28 12:15:02 -0400

    Add additional capabilities to script for packages present/not present scenarios.

--- files/rpm-query-oracle-vendor.sh
+++ files/rpm-query-oracle-vendor.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-rpm -qa --queryformat '%{vendor}:%{name}\n' | grep 'Oracle America'
+rpm -qa --queryformat '%{vendor}:%{name}\n' > /tmp/rpm_vendor_list.out
+RPM_VENDOR_LIST=$(grep 'Oracle America' /tmp/rpm_vendor_list.out)
+if [[ -z "$RPM_VENDOR_LIST" ]]; then
+    echo "No Oracle America packages present"
+else
+    cat /tmp/rpm_vendor_list.out
+fi
